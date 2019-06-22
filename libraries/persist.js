b46869aad9da11c8e03c792b171afb56034437a8
Nate Finger <natefinger@Nates-MacBook-Pro.local>
2019-06-21 21:06:56 -0400

    does local storage work?

--- libraries/persist.js
+++ libraries/persist.js
@@ -1,24 +1,25 @@
 // @flow
-import { parseCookies } from 'nookies';
-import Cookie from 'js-cookie';
-import { addMonths } from 'date-fns';
+// import { parseCookies } from 'nookies';
+// import Cookie from 'js-cookie';
+// import { addMonths } from 'date-fns';
 
 export default class persist {
   static get ACCESS_TOKEN_KEY(): string {
     return 'authToken';
   }
 
-  static async willGetAccessToken(ctx) {
-    return parseCookies(ctx, 'authToken');
+  // static async willGetAccessToken(ctx) {
+  static async willGetAccessToken() {
+    // return parseCookies(ctx, 'authToken');
+    return localStorage.getItem('authToken');
   }
 
   static async willSetAccessToken(value: string) {
-    return Cookie.set('authToken', value, {
-      expires: addMonths(new Date(), 12)
-    });
+    return localStorage.setItem('authToken', value);
   }
 
   static async willRemoveAccessToken() {
-    return Cookie.remove('authToken');
+    // return Cookie.remove('authToken');
+    return localStorage.removeItem('authToken');
   }
 }
